c543505cc2c0d70ab990b77b94854449fa87503c
Mario Vilas <mvilas@gmail.com>
2018-11-06 11:16:40 +0100

    Reduced file size (approx. 360k -> 36k)

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -100,11 +100,21 @@ except Exception:
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'
 
 # Load the list of valid user agents from the install folder.
-install_folder = os.path.abspath(os.path.split(__file__)[0])
-user_agents_file = os.path.join(install_folder, 'user_agents.txt')
 try:
-    with open(user_agents_file) as fp:
-        user_agents_list = [_.strip() for _ in fp.readlines()]
+    install_folder = os.path.abspath(os.path.split(__file__)[0])
+    try:
+        user_agents_file = os.path.join(install_folder, 'user_agents.txt.gz')
+        import gzip
+        fp = gzip.open(user_agents_file, 'rb')
+        try:
+            user_agents_list = [_.strip() for _ in fp.readlines()]
+        finally:
+            fp.close()
+            del fp
+    except Exception:
+        user_agents_file = os.path.join(install_folder, 'user_agents.txt')
+        with open(user_agents_file) as fp:
+            user_agents_list = [_.strip() for _ in fp.readlines()]
 except Exception:
     user_agents_list = [USER_AGENT]
 
